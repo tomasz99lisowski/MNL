4f69a893ac845096dd11912925a3c6e3c145a82a
tomasz99lisowski <tomasz.lisowski99@gmail.com>
2025-04-18 14:46:49 +0200

    Second practical fifth taks solved

--- Lab4/Zad7/fzero_results.m
+++ Lab4/Zad7/fzero_results.m
@@ -5,7 +5,7 @@ function [x1, y1, x2, y2] = fzero_results()
 % y1 - wartość funkcji tan wyznaczona dla argumentu x1
 % y2 - wartość funkcji tan wyznaczona dla argumentu x2
 
-options = optimset('Display','iter')
+options = optimset('Display','iter');
 x1 = fzero(@tan,4.5, options);
 x2 = fzero(@tan,6.0, options);
 y1 = tan(x1);
